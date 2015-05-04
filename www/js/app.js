638bb45e52714654ed80af6535bb35897ec82f72
Andy Mayer <andy@yoomee.com>
2015-05-04 20:01:34 +0100

    Add controller to view tasks

--- www/js/app.js
+++ www/js/app.js
@@ -17,3 +17,14 @@ angular.module('todo', ['ionic'])
     }
   });
 })
+
+// Controllers
+
+.controller('TodoCtrl', function($scope) {
+  $scope.tasks = [
+    { title: 'Collect coins' },
+    { title: 'Eat mushrooms' },
+    { title: 'Get high enough to grab the flag' },
+    { title: 'Find the Princess' }
+  ];
+})
